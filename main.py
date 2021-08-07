bfa4840220200fcc56fcb4deec86306180cc5a6d
Kyle <Kyle.J.Weidner-1@ou.edu>
2021-08-07 18:57:33 -0500

    Added read_items to return list of all autions

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI
 from item import Item
 
 app = FastAPI()
+
+# Advanced database system
 items = []
 
 @app.get("/")
@@ -15,8 +17,17 @@ def post_item(item_name: str, starting_bid: float):
 
     return temp.JSONResponse()
 
+@app.get("/items/")
+def read_items():
+    out = {}
+    
+    for i in range(0, len(items)):
+        out[i] = items[i].JSONResponse()
+    
+    return out
+
 @app.get("/items/{item_id}")
-def read_item(item_id: int):
+def read_item_by_id(item_id: int):
     if item_id > len(items)-1 or item_id < 0:
         return {"message": "Index out of bounds"}
     
