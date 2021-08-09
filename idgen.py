e512290c5f6549b58c7aa66dc7cad1bee8f8a6ae
Kyle <Kyle.J.Weidner-1@ou.edu>
2021-08-08 22:11:17 -0500

    New id scheme implmented

--- idgen.py
+++ idgen.py
@@ -1,8 +1,4 @@
 import random
 
-class IDGen():
-    def __init__(self):
-        self.id = self.genID()
-
-    def genID(self):
-        return random.randint(100000, 999999)
+def genID(self):
+    return random.randint(100000, 999999)
--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 from fastapi import FastAPI
 from item import Item
+import idgen
 
 app = FastAPI()
 
 # Advanced database system
 items = []
 
-def inBounds(x):
-    if x > -1 and x < len(items):
-        return True
-    return False
+def getIndexFromID(id):
+    for i in range(0, len(items)):
+        if items[i].getID() == id:
+            return i
+    
+    return -1
 
 @app.get("/")
 def read_root():
@@ -17,7 +20,7 @@ def read_root():
 
 @app.post("/items/")
 def post_item(item_name: str, starting_bid: float):
-    temp = Item(len(items), item_name, starting_bid)
+    temp = Item(idgen.genID(), item_name, starting_bid)
     items.append(temp)
 
     return temp.JSONResponse()
@@ -33,25 +36,25 @@ def read_items():
 
 @app.get("/items/{item_id}")
 def read_item_by_id(item_id: int):
-    if inBounds(item_id):
-        return items[item_id].JSONResponse()
+    if getIndexFromID(item_id) > -1:
+        return items[getIndexFromID(item_id)].JSONResponse()
 
-    return {"message": "Index out of bounds"}
+    return {"message": "Item not found"}
 
 @app.delete("/items/{item_id}")
 def del_item_by_id(item_id: int):
-    if inBounds(item_id):
-        del items[item_id]
+    if getIndexFromID(item_id) > -1:
+        del items[getIndexFromID(item_id)]
         return {"deleted": item_id}
     
-    return {"message": "Index out of bounds"}
+    return {"message": "Item not found"}
     
 @app.put("/items/{item_id}")
 def update_item_name(item_id: int, new_name: str):
-    if inBounds(item_id):
+    if getIndexFromID(item_id) > -1:
         if new_name != "":
-            items[item_id].setName(new_name)
-            return items[item_id].JSONResponse()
+            items[getIndexFromID(item_id)].setName(new_name)
+            return items[getIndexFromID(item_id)].JSONResponse()
         return {"message": "Item name cannot be empty"}
     
-    return {"message": "Index out of bounds"}
+    return {"message": "Item not found"}
